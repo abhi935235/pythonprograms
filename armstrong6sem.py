6f264b3f9e8a47002b522cec3c4b74a0fe408771
Abhishek Kachhwaha <abhi935235@gmail.com>
2019-12-11 09:33:27 +0530

    Update armstrong6sem.py

--- armstrong6sem.py
+++ armstrong6sem.py
@@ -11,5 +11,20 @@ if number == sum:
     print("Armstrong number")
 else:
     print("Not an armstrong")
+    
+#version 1.0
+# cook your dish here
+sum=0
+print("Armstrong between 0 to 1000 are:")
+for i in range(1000):
+    temp = i
+    number=temp
+    while temp > 0 :
+        digit = temp % 10
+        sum += digit ** 3
+        temp = temp//10
+    if number == sum:
+        print(number,end=" ")
+    sum=0
 
     

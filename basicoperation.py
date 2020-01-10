e1d723c536af4c120388fddc1663596a9f25ba0f
Abhishek Kachhwaha <abhi935235@gmail.com>
2020-01-10 11:44:27 +0530

    Create basicoperation.py

---
+++ basicoperation.py
@@ -0,0 +1,51 @@
+a=123 # integer 
+b=123.2 #float
+c="abhishek" #string
+d=None #none
+
+print(type(a),type(b),type(c),type(d))
+
+'''
+This is a 
+multi line 
+comment
+'''
+
+e=input("Enter name:")
+
+float(a)
+
+print(type(a))
+
+print(e[0:]) #slicing
+print(e[0:4]) # slicing using positive index
+print(e[-1]) #last character
+print(e[-1:-4]) # slicing using negative index 
+print(r[::2])  #hoping
+print(e[::-1]) #reverse
+print(e[::-2]) #reverse and hop
+print(len(e)) #length
+
+
+["Abhishek","Kachhwaha"]
+b=["CSE","COE"]
+'''
+#b= input("Enter college name:")
+print(a.upper())
+print(a.replace("Abhi","abhi"))
+print(a.count("e"))
+a=a.split()
+a=' '.join(a)
+print(a)
+'''
+print(a+b)
+print(a*3)
+print("Abhi" in a[0])
+
+'''
+what is python, why it is used widely and what are it's features?
+Explain libraries of python (num,pi,panda)?
+write a python program to perform basic concepts?
+datatype,type,comment,input,typecasting,slicing['single','double'],length,indexing,join,+,in,*
+
+'''
